3b07e4e9773c0cf1f091d02ea3a3478338ed4526
Mike West <mister270@gmail.com>
2018-05-15 00:23:52 -0700

    wip

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,3 @@
-// these hold references to html elements
-// var count;
-// var wins;
-// var losses;
-// var gem0;
-// var gem1;
-// var gem2;
-// var gem3;
-// var running_score;
 var count = 0;
 var wins = 0;
 var losses = 0;
@@ -14,16 +5,29 @@ var runningScore = 0;
 var randomNumber = 0;
 var gemValues = [0, 0, 0, 0];
 
-// document must be loaded
-function updateHtml() {
-    $("#count").text(count);
-    $("#wins").text(wins);
-    $("losses").text(losses);
-    $("running-score").text(running_score);
-}
 
-function randomValues() {
-    for (i==0; i<gemValues.length; ++i) {
-        rnd = Math.random() * (12 - 1) + 1;
+$(document).ready(function() {
+    function updateHtml() {
+        $("#count").text(count);
+        $("#wins").text(wins);
+        $("#losses").text(losses);
+        $("#running-score").text(runningScore);
+        $("#rnd-num").text(randomNumber);
     }
-}
+    
+    function randomValues() {
+        for (var i=0; i<gemValues.length; ++i) {
+            gemValues[i] = Math.floor(Math.random() * (12 - 1) + 1);
+        }
+        randomNumber = Math.floor(Math.random() * (120 -19) + 19);
+    }
+
+    $(".gem").on("click", function() {
+        value = gemValues[$(this).attr('value')];
+        runningScore += value;
+        updateHtml();
+    });
+
+    randomValues();
+    updateHtml();
+});
